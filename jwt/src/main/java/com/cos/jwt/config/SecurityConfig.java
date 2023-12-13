2006c5023e51db133ec3e880e1251ba9a3ddd595
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2023-12-13 22:11:23 +0900

    필터 테스트

--- jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -19,14 +23,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //스프링 시큐리티 filter chain이 내가 만든 필터보다 먼저 실행되므로, MyFilter3이 시큐리티 필터 체인보다
+        //먼저 실행되게 하고 싶으면 아래와 같이 만든다. 
+        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
         http.csrf(AbstractHttpConfigurer::disable);
-
         //세션 만들지 않기.
         http
                 .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilter(corsFilter)
                 .formLogin((formLogin) -> formLogin.disable())
-                .httpBasic((httpBasic) -> httpBasic.disable())
+                .httpBasic((httpBasic) -> httpBasic.disable()) //Bearer 방식을 사용하기 위해 basic 인증 비활성화
                 .authorizeHttpRequests((authorize) ->
                         authorize
                                 .requestMatchers("/api/v1/user/**").hasAnyRole("ADMIN", "MANAGER", "USER")
